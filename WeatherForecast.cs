9908a6d7b6314fd5e9ef5d9dc8307b99ae3867ca
Kuchko Sofiia <kuchko.sofiia@gmail.com>
2023-03-16 22:19:55 +0200

    first commit

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace ReenbitCamp_TestTask_backend
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
