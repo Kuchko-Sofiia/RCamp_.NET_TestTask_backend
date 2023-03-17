b473fbe2e4b4d5ef7d0da670e0a8c07b6d8e3dad
Kuchko Sofiia <kuchko.sofiia@gmail.com>
2023-03-17 21:18:11 +0200

    Main controller has been added.

---
+++ Controllers/MainController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReenbitCamp_TestTask_backend.Controllers
+{
+    [Route("[controller]")]
+    public class MainController : Controller
+    {
+        private readonly ILogger<MainController> _logger;
+
+        public MainController(ILogger<MainController> logger)
+        {
+            _logger = logger;
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFile([FromForm] IFormFile file, [FromForm] string? email)
+        {
+            //TODO: implement the action
+            return Ok($"Done. FileName: {file.FileName}; Email: {email}");
+        }
+    }
+}
